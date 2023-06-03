ddef6ac7d9194f232d090391bc0afe32ea25fc42
ic3surf3r <novis.joao@gmail.com>
2023-06-03 15:25:03 +0200

    mostly working

--- app.js
+++ app.js
@@ -1,21 +1,23 @@
 const buttons = document.querySelector(".buttons");
 const display = document.getElementById("display");
 const pluseminus = document.getElementById("plusOrMinus");
-let value1;
+let value1 = "";
+let value2 = "";
+let operator = "";
+let awaitOperation = false;
 buttons.addEventListener("click", doSomething);
-
-console.log(value1);
 
 function doSomething(e) {
   const classList = e.target.classList;
   const value = e.target.value;
   switch (true) {
     case classList.contains("number"):
-      if (value1 === undefined || value1 === "") {
-        numberToDisplay(e.target.value);
-      } else {
+      if (awaitOperation) {
+        awaitOperation = false;
         display.innerText = "0";
-        numberToDisplay(e.target.value);
+        numberToDisplay(value);
+      } else {
+        numberToDisplay(value);
       }
       break;
     case classList.contains("clear"):
@@ -23,10 +25,86 @@ function doSomething(e) {
       pluseminus.innerText = "";
       break;
     case classList.contains("plusminus"):
+      if (awaitOperation) {
+        display.innerText = "0";
+      }
       if (pluseminus.innerText === "") {
         pluseminus.innerText = "-";
       } else {
         pluseminus.innerText = "";
+      }
+      break;
+
+    case classList.contains("equals"):
+      value2 = checkIfNegativeValue(display.innerText);
+      performOperation(value1, value2, operator);
+      value1 = "";
+      value2 = "";
+      operator = "";
+      awaitOperation = false;
+      break;
+    case classList.contains("addition"):
+      if (value1 === "") {
+        value1 = checkIfNegativeValue(display.innerText);
+        operator = "add";
+        awaitOperation = true;
+      } else if (awaitOperation) {
+        operator = "add";
+      } else {
+        value2 = checkIfNegativeValue(display.innerText);
+        performOperation(value1, value2, operator);
+        value1 = checkIfNegativeValue(display.innerText);
+        value2 = "";
+        operator = "add";
+        awaitOperation = true;
+      }
+      break;
+    case classList.contains("subtraction"):
+      if (value1 === "") {
+        value1 = checkIfNegativeValue(display.innerText);
+        operator = "subtract";
+        awaitOperation = true;
+      } else if (awaitOperation) {
+        operator = "subtract";
+      } else {
+        value2 = checkIfNegativeValue(display.innerText);
+        performOperation(value1, value2, operator);
+        value1 = checkIfNegativeValue(display.innerText);
+        value2 = "";
+        operator = "subtract";
+        awaitOperation = true;
+      }
+      break;
+    case classList.contains("multiplication"):
+      if (value1 === "") {
+        value1 = checkIfNegativeValue(display.innerText);
+        operator = "multiply";
+        awaitOperation = true;
+      } else if (awaitOperation) {
+        operator = "multiply";
+      } else {
+        value2 = checkIfNegativeValue(display.innerText);
+        performOperation(value1, value2, operator);
+        value1 = checkIfNegativeValue(display.innerText);
+        value2 = "";
+        operator = "multiply";
+        awaitOperation = true;
+      }
+      break;
+    case classList.contains("division"):
+      if (value1 === "") {
+        value1 = checkIfNegativeValue(display.innerText);
+        operator = "divide";
+        awaitOperation = true;
+      } else if (awaitOperation) {
+        operator = "divide";
+      } else {
+        value2 = checkIfNegativeValue(display.innerText);
+        performOperation(value1, value2, operator);
+        value1 = checkIfNegativeValue(display.innerText);
+        value2 = "";
+        operator = "divide";
+        awaitOperation = true;
       }
       break;
   }
@@ -37,6 +115,7 @@ function performOperation(value1, value2, operation) {
   switch (operation) {
     case "add":
       result = value1 + value2;
+      console.log(value1, value2, operation);
       checkIfNegative(result);
       break;
     case "subtract":
@@ -60,10 +139,19 @@ function checkIfNegative(result) {
   if (result < 0) {
     pluseminus.innerText = "-";
     result = result * -1;
-    display.innerText = result;
+    display.innerText = `${result}`;
   } else {
     pluseminus.innerText = "";
-    display.innerText = result;
+    display.innerText = `${result}`;
+  }
+}
+
+function checkIfNegativeValue(value) {
+  if (pluseminus.innerText === "-") {
+    const res = +value * -1;
+    return res;
+  } else {
+    return +value;
   }
 }
 
